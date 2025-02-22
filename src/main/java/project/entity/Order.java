095fa489f1931b1f94051d3a3e79d7e349ba9f4e
tolya <tolya@gmx.de>
2025-02-22 13:08:10 +0100

    order db

---
+++ src/main/java/project/entity/Order.java
@@ -0,0 +1,35 @@
+package project.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id"
+    )
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItems> items;
+
+    private BigDecimal totalPrice;
+}
---
+++ src/main/java/project/entity/OrderItems.java
@@ -0,0 +1,40 @@
+package project.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "order_items")
+public class OrderItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "order_id",
+            referencedColumnName = "id"
+    )
+    @JsonIgnore
+    private Order order;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "product_id",
+            referencedColumnName = "id"
+    )
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal itemPrice;
+}
